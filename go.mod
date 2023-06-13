ac57ad2af679ea501caa289a0748fff9bbda7c3d
NickKlode <divinitylife20@mail.ru>
2023-06-13 17:17:14 +0300

    add in-memory parallel access protection

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgx/v4 v4.18.1
+	github.com/joho/godotenv v1.5.1
 	github.com/spf13/viper v1.16.0
 )
 
@@ -19,7 +20,6 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
--- internal/storage/inmemory/inmemory.go
+++ internal/storage/inmemory/inmemory.go
@@ -1,20 +1,25 @@
 package inmemory
 
 import (
+	"sync"
+
 	"github.com/nickklode/ozon-urlshortener/internal/storage"
 	"github.com/nickklode/ozon-urlshortener/internal/utils/generator"
 	"github.com/nickklode/ozon-urlshortener/internal/utils/validator"
 )
 
 type Store struct {
-	m map[string]storage.Links
+	m  map[string]storage.Links
+	mu sync.Mutex
 }
 
 func New() *Store {
-	return &Store{m: map[string]storage.Links{}}
+	return &Store{m: map[string]storage.Links{}, mu: sync.Mutex{}}
 }
 
 func (s *Store) CreateToken(orig string) (string, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	err := validator.ValidateURL(orig)
 	if err != nil {
 		return "", err
@@ -22,10 +27,12 @@ func (s *Store) CreateToken(orig string) (string, error) {
 	sur, ok := s.m[orig]
 	if ok {
 		return sur.Token, nil
+	} else {
+		sur = storage.Links{OriginalUrl: orig, Token: generator.GenerateToken()}
+		s.m[orig] = sur
+		return sur.Token, nil
 	}
-	sur = storage.Links{OriginalUrl: orig, Token: generator.GenerateToken()}
-	s.m[orig] = sur
-	return sur.Token, nil
+
 }
 
 func (s *Store) GetByToken(token string) (string, error) {
